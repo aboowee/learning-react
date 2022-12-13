bc398f24db5d9a918417335e7f9b2151d5e0f6cc
Anthony Bui <anthonyb50@gmail.com>
2022-12-12 20:27:33 -0500

    React GroceryList Complete

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+  return (
+    <li style={style} onClick={() => setIsDone(!isDone)}>
+      {props.food}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.foods.map((food) => (
+      <GroceryListItem food={food} />
+    ))}
+  </ul>
+);
+
+const Grocery = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList foods={['Dairy', 'Egg', 'Bread']}/>
+  </div>
+  );
+
+ReactDOM.render(<Grocery />, document.getElementById('app'));
+
+
+
+// const TodoListItem = (props) => (
+//   <li>{props.todo}</li>
+// )
+
+// const TodoList = (props) => (
+//   <ul>
+//     {props.todos.map((todo) => (
+//       <TodoListItem todo={todo} />
+//     ))}
+//   </ul>
+// );
+
+// const App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+
+// ReactDOM.render(<App />, document.getElementById('app'));
